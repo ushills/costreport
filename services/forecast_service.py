fa65417e9e8dad2cec05c74e0da22ab4041e95cd
ushills <web@ushills.co.uk>
2019-09-25 10:12:25 +0100

    added flexbox colums to forecast.html

--- services/forecast_service.py
+++ services/forecast_service.py
@@ -1,17 +1,24 @@
 def get_forecast_detail():
     return [
         {
-            "cost_code": "5A",
+            "cost_code": "C34223",
             "cost_code_description": "Space Heating & Air Treatment",
             "cost_allowance": 56000,
             "cost_forecast": 45000,
             "cost_remaining": 56000 - 45000,
         },
         {
-            "cost_code": "5H",
+            "cost_code": "C13353",
             "cost_code_description": "Electrical Installations",
             "cost_allowance": 4000,
             "cost_forecast": 4500,
             "cost_remaining": 4000 - 4500,
+        },
+        {
+            "cost_code": "C72653",
+            "cost_code_description": "Boilers",
+            "cost_allowance": 26323,
+            "cost_forecast": 26325,
+            "cost_remaining": 26323 - 26325,
         },
     ]
