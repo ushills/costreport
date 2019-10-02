02053599aafdf4e1b43b3877f959b010e4da7384
ushills <web@ushills.co.uk>
2019-10-02 13:49:20 +0100

    corrected ForeignKey

--- data/transactions.py
+++ data/transactions.py
@@ -12,10 +12,10 @@ class Transaction(SqlAlchemyBase):
     id = sa.Column(sa.Integer, primary_key=True)
     created_date = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
     project_id = sa.Column(
-        sa.Integer, ForeignKey("projects.project_id"), nullable=False, index=True
+        sa.Integer, sa.ForeignKey("projects.project_id"), nullable=False, index=True
     )
     cost_code_id = sa.Column(
-        sa.Integer, ForeignKey("costcodes.costcode_id", nullable=False, index=True)
+        sa.Integer, sa.ForeignKey("costcodes.costcode_id", nullable=False, index=True)
     )
     value = sa.Column(sa.Decimal(9, 2), nullable=False)
     note = sa.Column(sa.String)
