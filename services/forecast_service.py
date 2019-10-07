65115170a5487708846f33a48f28f531651b1f02
ushills <web@ushills.co.uk>
2019-10-07 16:49:25 +0000

    working on forecast template & service

--- services/forecast_service.py
+++ services/forecast_service.py
@@ -1,24 +1,19 @@
+# def get_forecast_detail():
+#     return [
+#         {
+#             "cost_code": "C34223",
+#             "cost_code_description": "Space Heating & Air Treatment",
+#             "cost_allowance": 56000,
+#             "cost_forecast": 45000,
+#         },
+#     ]
+from costreport.data.costcodes import Costcodes
+
+
 def get_forecast_detail():
-    return [
-        {
-            "cost_code": "C34223",
-            "cost_code_description": "Space Heating & Air Treatment",
-            "cost_allowance": 56000,
-            "cost_forecast": 45000,
-        },
-        {
-            "cost_code": "C13353",
-            "cost_code_description": "Electrical Installations",
-            "cost_allowance": 4000,
-            "cost_forecast": 4500,
-        },
-        {
-            "cost_code": "C72653",
-            "cost_code_description": "Boilers",
-            "cost_allowance": 26323,
-            "cost_forecast": 26324,
-        },
-    ]
+    cost_code_detail = Costcodes.query.order_by(Costcodes.costcode.asc()).all()
+    print(cost_code_detail)
+    return cost_code_detail
 
 
 def get_summary_detail():
--- services/projects_service.py
+++ services/projects_service.py
@@ -3,5 +3,5 @@ from costreport.data.projects import Project
 
 def get_project_list():
     projects = Project.query.order_by(Project.project_id.desc()).all()
-    # print(projects)
+    print(projects)
     return projects
