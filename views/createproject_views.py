7792d1ab65b6e0b867f1424e99b1db0e50959e6f
ushills <web@ushills.co.uk>
2019-10-09 16:06:41 +0000

    Working on forms

--- views/createproject_views.py
+++ views/createproject_views.py
@@ -7,21 +7,21 @@ from wtforms.validators import DataRequired
 blueprint = flask.Blueprint("createproject", __name__, template_folder="templates")
 
 
-class CreateProjectForm(FlaskForm):
+class CreateProjectForm:
     project_reference = StringField(validators=[DataRequired()])
     project_name = StringField(validators=[DataRequired()])
-    submit = SubmitField()
 
 
 @blueprint.route("/admin/createproject", methods=["GET"])
 def createproject_get():
-    form = CreateProjectForm()
-    print(form.project_reference.data, form.project_name.data)
-    return flask.render_template("admin/createproject.html", form=form)
+    return flask.render_template("admin/createproject.html")
 
 
 @blueprint.route("/admin/createproject", methods=["POST"])
 def createproject_post():
-    form = CreateProjectForm()
-    print(form.project_reference.data, form.project_name.data)
+    request = flask.request
+    project_reference = request.project_reference
+    project_name = request.project_name
+    print(project_reference)
+    print(project_name)
     return flask.render_template("admin/createproject.html", form=form)
