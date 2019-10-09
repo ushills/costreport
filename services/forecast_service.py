7d4cd1bae472d19527b45ccdc87ccefbef2128bc
ushills <web@ushills.co.uk>
2019-10-09 13:59:22 +0000

    Reverted forecast_service

--- services/forecast_service.py
+++ services/forecast_service.py
@@ -1,19 +1,21 @@
-# def get_forecast_detail():
-#     return [
-#         {
-#             "cost_code": "C34223",
-#             "cost_code_description": "Space Heating & Air Treatment",
-#             "cost_allowance": 56000,
-#             "cost_forecast": 45000,
-#         },
-#     ]
-from costreport.data.costcodes import Costcodes
-
-
 def get_forecast_detail():
-    cost_code_detail = Costcodes.query.order_by(Costcodes.costcode.asc()).all()
-    print(cost_code_detail)
-    return cost_code_detail
+    return [
+        {
+            "cost_code": "C34223",
+            "cost_code_description": "Space Heating & Air Treatment",
+            "cost_allowance": 56000,
+            "cost_forecast": 45000,
+        }
+    ]
+
+
+# from costreport.data.costcodes import Costcodes
+
+
+# def get_forecast_detail():
+#     cost_code_detail = Costcodes.query.order_by(Costcodes.costcode.asc()).all()
+#     print(cost_code_detail)
+#     return cost_code_detail
 
 
 def get_summary_detail():
