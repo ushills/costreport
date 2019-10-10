0e10ce86261f0e46f5bc24ab06af0c4e3538b012
ushills <web@ushills.co.uk>
2019-10-10 14:35:45 +0100

    Createproject commits to database

--- services/admin_services.py
+++ services/admin_services.py
@@ -1,2 +1,11 @@
+from costreport.app import db
+from costreport.data.projects import Project
+
+
 def create_project(data):
-    
+    p = Project()
+    p.project_code = data["project_code"]
+    p.project_name = data["project_name"]
+    db.session.add(p)
+    db.session.commit()
+    print(data)
--- views/createproject_views.py
+++ views/createproject_views.py
@@ -3,13 +3,14 @@ from flask import request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from costreport.services.admin_services import create_project
 
 
 blueprint = flask.Blueprint("createproject", __name__, template_folder="templates")
 
 
 class CreateProjectForm(FlaskForm):
-    project_reference = StringField("Project Reference", validators=[DataRequired()])
+    project_code = StringField("Project Reference", validators=[DataRequired()])
     project_name = StringField("Project Name", validators=[DataRequired()])
 
 
@@ -21,12 +22,13 @@ def createproject_get():
 
 @blueprint.route("/admin/createproject", methods=["POST"])
 def createproject_post():
-    # project_reference = request.form.get("project_reference")
-    # project_name = request.form.get("project_name")
-    # print(project_reference, project_name)
     form = CreateProjectForm()
-    print(form.project_reference.data, form.project_name.data)
+    data = {
+        "project_code": form.project_code.data,
+        "project_name": form.project_name.data,
+    }
     if form.validate_on_submit():
+        create_project(data)
         return flask.redirect("createproject/success")
     return flask.render_template("admin/createproject.html", form=form)
 
