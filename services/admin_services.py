6a3375e59d358dabb682265627718a07bc64c640
ushills <web@ushills.co.uk>
2019-10-12 10:18:40 +0000

    Corrected filter for create_costcode to use correct project.

--- services/admin_services.py
+++ services/admin_services.py
@@ -43,9 +43,18 @@ def create_costcode(data):
 
 
 def get_costcodes(project_code):
+    # print(project_code)
+    # print(Project.project_code.)
+    # costcodes = (
+    #     Costcodes.query.filter(Project.project_code == project_code)
+    #     .order_by(Costcodes.costcode.asc())
+    #     .all()
+    # )
     costcodes = (
-        Costcodes.query.filter(Project.project_code == project_code)
+        Costcodes.query.filter(Project.id == Costcodes.project_id)
+        .filter(Project.project_code == project_code)
         .order_by(Costcodes.costcode.asc())
         .all()
     )
+    print(costcodes)
     return costcodes
