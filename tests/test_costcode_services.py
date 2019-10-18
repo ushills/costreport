4a11d7b9448f3df7e25c8412f37fdba751136051
ushills <web@ushills.co.uk>
2019-10-18 13:37:35 +0100

    added costcode tests

--- tests/test_costcode_services.py
+++ tests/test_costcode_services.py
@@ -32,3 +32,34 @@ def test_add_a_costcode(client):
 
 def test_get_costcodes(client):
     assert admin_services.get_costcodes("12345")[0].costcode == "C1000"
+
+
+def test_get_costcode_data(client):
+    costcode_data = admin_services.get_costcode_data(
+        project_code="12345", costcode="C1000"
+    )
+    assert costcode_data[0].costcode_description == "Costcode A"
+    assert costcode_data[0].costcode_category == "Category A"
+
+
+def test_check_if_costcode_exists(client):
+    assert (
+        admin_services.check_if_costcode_exists(project_code="12345", costcode="C1000")
+        is True
+    )
+
+
+def test_check_if_project_does_not_exist(client):
+    assert (
+        admin_services.check_if_costcode_exists(project_code="76543", costcode="C1000")
+        is False
+    )
+
+
+def test_check_if_costcode_does_not_exist(client):
+    assert (
+        admin_services.check_if_costcode_exists(
+            project_code="12345", costcode="not exist"
+        )
+        is False
+    )
