92e341aae9eeebbe01b01c15b3350619aba3b2f0
ushills <web@ushills.co.uk>
2019-11-11 16:45:14 +0000

    import_default_costcodes_view in progress

--- costreport/views/import_default_costcodes_view.py
+++ costreport/views/import_default_costcodes_view.py
@@ -7,6 +7,7 @@ from costreport.app import app
 import flask
 from flask import request
 from flask_wtf import FlaskForm
+from wtforms import SubmitField
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import CombinedMultiDict
@@ -23,42 +24,50 @@ blueprint = flask.Blueprint(
 
 
 class ImportDefaultCostcodesForm(FlaskForm):
+    # Need to work around FileRequired not working with validate_on_submit
+    # consider splitting into 2 forms
     csvfile = FileField(
         validators=[FileRequired(), FileAllowed(["csv"], "csv files only!")]
     )
+    upload_button = SubmitField()
+    save_button = SubmitField()
 
 
-@blueprint.route("/upload_costcodes", methods=["GET", "POST"])
-def upload_default_costcodes():
+@blueprint.route("/upload_costcodes", methods=["GET"])
+def upload_default_costcodes_get():
     form = ImportDefaultCostcodesForm(CombinedMultiDict((request.files, request.form)))
-    if form.validate_on_submit():
-        f = form.csvfile.data
-        filename = secure_filename(f.filename)
-        filepath = os.path.join(app.instance_path, "csv_uploads", filename)
-        f.save(filepath)
-        try:
-            csvdata = admin_services.read_costcodes_from_csv(filepath)
-            costcodes = json.dumps(csvdata)
-            # breakpoint()
-            # os.remove(filepath)
-        except:
-            os.remove(filepath)
-        return flask.redirect(
-            flask.url_for(
-                "import_default_costcodes.view_default_costcodes", costcodes=costcodes
-            )
-        )
     return flask.render_template("admin/upload_default_costcodes.html", form=form)
 
 
-@blueprint.route("/view_uploaded_costcodes", methods=["GET", "POST"])
-def view_default_costcodes():
-    costcodes = flask.request.args.get("costcodes")
-    costcodes = json.loads(costcodes)
+@blueprint.route("/upload_costcodes", methods=["POST"])
+def upload_default_costcodes_post():
+    form = ImportDefaultCostcodesForm(CombinedMultiDict((request.files, request.form)))
+    if form.validate_on_submit():
+        # breakpoint()
+        if form.upload_button.data:
+            costcodes = import_csv(form.csvfile.data)
+            print("upload button pressed")
+            return flask.render_template(
+                "admin/upload_default_costcodes.html", form=form, costcodes=costcodes
+            )
+        elif form.save_button.data:
+            print("save button pressed")
+            # TODO process to commit to the database
+            return flask.redirect(flask.url_for("projects.projects"))
+        else:
+            print("no button pressed")
 
-    if costcodes is None:
-        flask.abort(404)
+    return flask.render_template("admin/upload_default_costcodes.html", form=form)
 
-    return flask.render_template(
-        "admin/view_uploaded_costcodes.html", costcodes=costcodes
-    )
+
+def import_csv(csv_file):
+    filename = secure_filename(csv_file.filename)
+    filepath = os.path.join(app.instance_path, "csv_uploads", filename)
+    csv_file.save(filepath)
+    try:
+        csvdata = admin_services.read_costcodes_from_csv(filepath)
+        costcodes = csvdata
+        os.remove(filepath)
+        return costcodes
+    except:
+        os.remove(filepath)
