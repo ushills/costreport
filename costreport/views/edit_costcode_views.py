c6df8267cf5cce0d29fc91f8798f11c41cd6f1aa
ushills <web@ushills.co.uk>
2019-11-27 12:05:42 +0000

    edit_costcode working

--- costreport/views/edit_costcode_views.py
+++ costreport/views/edit_costcode_views.py
@@ -2,8 +2,10 @@ import flask
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
+import costreport.services.admin_services as admin_services
 import costreport.services.costcode_services as costcode_services
-from costreport.services.projects_service import check_if_project_exists
+import costreport.services.project_view_services as project_view_services
+import costreport.services.projects_service as projects_service
 
 
 blueprint = flask.Blueprint(
@@ -12,11 +14,9 @@ blueprint = flask.Blueprint(
 
 
 class CreateCostcodeForm(FlaskForm):
-    costcode = StringField("Costcode Reference", validators=[DataRequired()])
-    costcode_description = StringField(
-        "Costcode Description", validators=[DataRequired()]
-    )
-    costcode_category = StringField("Costcode category")
+    # costcode = StringField("Costcode Reference", validators=[DataRequired()])
+    costcode_description = StringField("Description", validators=[DataRequired()])
+    costcode_category = StringField("Category")
 
 
 @blueprint.route("/edit_costcode", methods=["GET"])
@@ -24,7 +24,7 @@ def edit_costcode_get():
     project_code = flask.request.args.get("project")
     costcode = flask.request.args.get("costcode")
     # check if project exists
-    if check_if_project_exists(project_code) is False:
+    if projects_service.check_if_project_exists(project_code) is False:
         flask.abort(404)
     # check if costcode exists
     if (
@@ -34,6 +34,8 @@ def edit_costcode_get():
         is False
     ):
         flask.abort(404)
+    # get project details
+    project_details = project_view_services.get_project_details(project_code)
     form = CreateCostcodeForm()
     # get costcode data
     costcode_data = costcode_services.get_costcode_data(
@@ -42,8 +44,7 @@ def edit_costcode_get():
     return flask.render_template(
         "admin/edit_costcode.html",
         form=form,
-        project=project_code,
-        costcode=costcode,
+        project_details=project_details,
         costcode_data=costcode_data,
     )
 
@@ -56,30 +57,20 @@ def edit_costcode_post():
     form = CreateCostcodeForm()
     data = {
         "project_code": project_code,
-        "costcode": form.costcode.data,
+        "costcode": costcode,
         "costcode_description": form.costcode_description.data,
         "costcode_category": form.costcode_category.data,
     }
     if form.validate_on_submit():
-        # check if the costcode already exists for the project_code
-        if costcode_services.check_if_costcode_exists(data):
-            flask.flash(
-                "Costcode " + form.costcode.data + " already exists", "alert-danger"
+        admin_services.update_costcode(data)
+        flask.flash("Costcode " + costcode + " edited", "alert-success")
+        return flask.redirect(
+            flask.url_for(
+                "edit_costcode.edit_costcode_get",
+                project=project_code,
+                costcode=costcode,
             )
-            return flask.redirect(
-                flask.url_for("edit_costcode.edit_costcode_get", project=project_code)
-            )
-        # commit the data to the database
-        else:
-            costcode_services.edit_costcode(data)
-            flask.flash("Costcode " + form.costcode.data + " created", "alert-success")
-            return flask.redirect(
-                flask.url_for("edit_costcode.edit_costcode_get", project=project_code)
-            )
+        )
     return flask.render_template(
-        "admin/edit_costcode.html",
-        form=form,
-        project=project_code,
-        costcodes=costcode,
-        costcode_data=costcode_data,
+        "admin/edit_costcode.html", form=form, costcode_data=costcode_data,
     )
